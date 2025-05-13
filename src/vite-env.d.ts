34f8df6f9edf6f6d6832945ff7190e98a5e45017
sofia-kindratyshyn <sofia25kind@gmail.com>
2025-05-13 11:43:30 +0300

    Changes in index.html

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+// vite.config.ts
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/01-ts-basics/', // ← назва твого репозиторію!
+});
